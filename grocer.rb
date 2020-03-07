680fc25a12062e577e7f8aa84c2e049e5424e68d
Radniel Padron <radnielpadron@gmail.com>
2020-03-07 23:52:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,16 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-  #  binding.pry
   
-  # i = 0
-  # while i < collection.length 
-  #   var = collection[i]
-    
-  # i += 1
-  # end
-  # binding.pry
-
     i = 0
   while i < collection.length
     var = collection[i]

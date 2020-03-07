9072e7e3def13b804f054089e5e0d65fe787c0c7
Radniel Padron <radnielpadron@gmail.com>
2020-03-07 23:59:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
-  binding.pry
+  # binding.pry
   
   
   
